800f55fe32d2f83f0e529ec76e21db13b1f4967c
rena <68492530+minahow3@users.noreply.github.com>
2025-01-08 14:04:42 +0000

    Nextアプリ作成、V0からフロントエンドインポート

---
+++ siori-pj/src/_app/page.jsx
@@ -0,0 +1,10 @@
+import { ContactForm } from '@/components/contact-form'
+
+export default function Page() {
+  return (
+    (<div className="flex min-h-svh items-center justify-center">
+      <ContactForm />
+    </div>)
+  );
+}
+
